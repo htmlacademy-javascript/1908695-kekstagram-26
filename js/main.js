ad830ab560148432b78bb803f45f7368b6ce8a47
olgadeveloper <madonovaolga@icloud.com>
2022-06-06 14:31:12 +0700

    arrow functions

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
-function getRandomInt(min, max) {
-  if ( max > min && max >= 0 && min >= 0) {
+const getRandomInt = (min, max) => {
+  if (max > min && max >= 0 && min >=0) {
     return Math.floor(Math.random() * (max - min)) + min;
   } if (min > max || max===min) {
     return 'Первый аргумент должен быть меньше второго, введите корректные параметры';
-  } else {
-    return 'В диапазоне нет подходящего числа, функция не может гарантировать верный результат';
-  }
-}
-getRandomInt(5, 8);
+  } return 'В диапазоне нет подходящего числа, функция не может гарантировать верный результат';
+};
 
-function getStringLength (string, maxlength) {
-  const stringLength = string.length;
-  return stringLength < maxlength || stringLength === maxlength;
-}
+getRandomInt(5, 8);
+const getStringLength = (string, maxlength) => string.length < maxlength || string.length === maxlength;
+
 getStringLength('hello', 10);
