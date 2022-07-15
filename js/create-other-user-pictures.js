43bf7dee2155acc63e4a0e5328d4251256ef08cb
olgadeveloper <madonovaolga@icloud.com>
2022-07-15 22:09:54 +0700

    filter-choice button toggle added

--- js/create-other-user-pictures.js
+++ js/create-other-user-pictures.js
@@ -1,6 +1,10 @@
 import {addThumbnailClickHandler} from './watch-big-pics.js';
+import {debounce} from './util.js';
+
 const otherUserPicturesList = document.querySelector('.pictures');
 const otherUserPicture = document.querySelector('#picture').content.querySelector('.picture');
+const filterChooser = document.querySelector('.img-filters');
+const filterButtons = filterChooser.querySelectorAll('.img-filters__button');
 
 const createPhotos = (otherUsersPhotoDescriptions) => {
   const otherUserPicturesListFragment = document.createDocumentFragment();
@@ -17,7 +21,20 @@ const createPhotos = (otherUsersPhotoDescriptions) => {
     otherUserPicturesListFragment.append(otherUserPictureElement);
   });
   otherUserPicturesList.append(otherUserPicturesListFragment);
+  filterChooser.classList.remove('img-filters--inactive');
 };
 
+const toggleFilterChooser = (chosenButton) => {
+  filterChooser.querySelector('.img-filters__button--active').classList.remove('img-filters__button--active');
+  chosenButton.classList.add('img-filters__button--active');
+};
+
+filterChooser.addEventListener('click', (evt) => {
+  const target = evt.target;
+  if (target.matches('.img-filters__button')) {
+    console.log('click');
+    toggleFilterChooser(target);
+  }
+});
 export {createPhotos};
 
--- js/util.js
+++ js/util.js
@@ -50,4 +50,13 @@ getStringLength('hello', 10);
 
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
-export {isEscapeKey, showAlert};
+const debounce = (callback, timeoutDelay = 500) =>{
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+export {isEscapeKey, showAlert, debounce};
